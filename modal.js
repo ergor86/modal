f7ff699d9aabe36397563334007afa1d6f1b06a8
André de Souza Rosa <andrepetsu@hotmail.com>
2023-10-02 21:47:28 -0300

    documentos base para modal

---
+++ modal.js
@@ -0,0 +1,5 @@
+var modal = function(){
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+}
